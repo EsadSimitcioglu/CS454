c3f89ffc4a74939a697da7145319676a7f602b38
Esat Simitçioğlu <esad.simitcioglu@ozu.edu.tr>
2021-10-15 01:05:33 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import csv
+import math
+
+
+class Class:
+    name = "";
+    ages = []
+    mean = 0
+    std = 0
+    priors = 0
+
+    def __init__(self, name):
+        self.name = name
+
+    def calculate(self):
+        self.calculateMean()
+        self.calculateStd()
+        self.calculatePrior()
+
+    def calculatePrior(self, lengthOfTheData):
+        self.priors = len(self.ages) / lengthOfTheData
+
+    def calculateMean(self):
+        sumOfAges = 0
+        for element in self.ages:
+            sumOfAges += int(element)
+        self.mean = sumOfAges / len(self.ages)
+
+    def calculateStd(self):
+        sumOfValues = 0
+        for element in self.ages:
+            sumOfValues += math.pow((int(element) - self.mean), 2)
+        self.std = math.sqrt(sumOfValues / 3)
+
+    def calculateLikelihhod(self, value):
+        powerSide = math.pow(math.e, (-1/2) * math.pow(((value- self.mean))/self.std), 2)
+        return (-1)/(self.std * math.sqrt(2*math.pi)) * powerSide
+
+
+class1 = Class("1")
+class2 = Class("2")
+class3 = Class("3")
+
+with open('training.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    isHeader = True
+    for row in csv_reader:
+        if isHeader:
+            print(f' Age : \t{row[0]} ; Class : {row[1]}')
+            isHeader = False
+            continue
+
+        if row[1] == class1.name:
+            class1.ages.append(row[0])
+        elif row[1] == class2.name:
+            class2.ages.append(row[0])
+        elif row[1] == class3.name:
+            class3.ages.append(row[0])
