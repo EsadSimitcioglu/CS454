5f406ea45fb03a8389bcc92a69c2435d140225e1
Esat Simitçioğlu <esad.simitcioglu@ozu.edu.tr>
2021-10-15 14:08:48 +0300

    Done

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ import math
 
 
 class Class:
-    name = "";
-    ages = []
-    mean = 0
-    std = 0
-    priors = 0
 
     def __init__(self, name):
         self.name = name
+        self.ages = []
+        self.mean = 0
+        self.std = 0
+        self.priors = 0
 
-    def calculate(self,lengthOfTheData):
+    def calculateProps(self,lengthOfTheData):
         self.calculateMean()
         self.calculateStd()
         self.calculatePrior(lengthOfTheData)
@@ -34,13 +33,19 @@ class Class:
 
     def calculateLikelihhod(self, value):
         powerSide = math.pow(math.e, (-1/2) * math.pow(((value- self.mean))/self.std,2))
-        return (-1)/(self.std * math.sqrt(2*math.pi)) * powerSide
+        return ((-1)/(self.std * math.sqrt(2*math.pi))) * powerSide
+
 
 class1 = Class("1")
 class2 = Class("2")
 class3 = Class("3")
 
-lineCount = 0;
+age1 = []
+age2 = []
+age3 = []
+
+
+lineCount = 0
 
 with open('training.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -57,10 +62,11 @@ with open('training.csv') as csv_file:
         elif row[1] == class3.name:
             class3.ages.append(row[0])
 
-class1.calculate(lineCount)
-class2.calculate(lineCount)
-class3.calculate(lineCount)
 
+
+class1.calculateProps(lineCount)
+class2.calculateProps(lineCount)
+class3.calculateProps(lineCount)
 
 approx = 26
 
@@ -68,14 +74,13 @@ likelihood1 = class1.calculateLikelihhod(approx)
 likelihood2 = class2.calculateLikelihhod(approx)
 likelihood3 = class3.calculateLikelihhod(approx)
 
+
 result1 = (likelihood1 * class1.priors) / ((likelihood1 * class1.priors) + (likelihood2 * class2.priors) + (likelihood3 * class3.priors))
 result2 = (likelihood2 * class2.priors) / ((likelihood1 * class1.priors) + (likelihood2 * class2.priors) + (likelihood3 * class3.priors))
 result3 = (likelihood3 * class3.priors) / ((likelihood1 * class1.priors) + (likelihood2 * class2.priors) + (likelihood3 * class3.priors))
 
+print(max(result1,result2,result3))
 
-print(result1)
-print(result2)
-print(result3)
 
 
 
