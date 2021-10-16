691ab503d020a632fed7c7dafe218dc233997d97
Esat Simitçioğlu <esad.simitcioglu@ozu.edu.tr>
2021-10-16 17:45:37 +0300

    2-a is complete

--- main.py
+++ main.py
@@ -35,6 +35,105 @@ class Class:
         powerSide = math.exp((-1/2) * math.pow(((value- self.mean))/self.std,2))
         return (1 / (self.std * math.sqrt(2 * math.pi))) * powerSide
 
+def createConfusionMatrice(class1,class2,class3):
+    trainingMatrix = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]
+
+    testingMatrix = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]
+
+    with open("training.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        isHeader = True
+        for row in csv_reader:
+            if isHeader:
+                isHeader = False
+                continue
+            likelihood1 = class1.calculateLikelihhod(int(row[0]))
+            likelihood2 = class2.calculateLikelihhod(int(row[0]))
+            likelihood3 = class3.calculateLikelihhod(int(row[0]))
+            resultOfClass1 = (likelihood1 * class1.priors) / (
+                    (likelihood1 * class1.priors) + (likelihood2 * class2.priors) + (likelihood3 * class3.priors))
+            resultOfClass2 = ((likelihood2 * class2.priors) / (
+                    (likelihood1 * class1.priors) + (likelihood2 * class2.priors) + (likelihood3 * class3.priors)))
+            resultOfClass3 = ((likelihood3 * class3.priors) / (
+                    (likelihood1 * class1.priors) + (likelihood2 * class2.priors) + (likelihood3 * class3.priors)))
+
+            maximumResult = max(resultOfClass1, resultOfClass2, resultOfClass3)
+
+            if (maximumResult == resultOfClass1):
+                if (row[1] == "1"):
+                    trainingMatrix[0][0] += 1
+                elif (row[1] == "2"):
+                    trainingMatrix[1][0] += 1
+                elif (row[1] == "3"):
+                    trainingMatrix[2][0] += 1
+            elif (maximumResult == resultOfClass2):
+                if (row[1] == "1"):
+                    trainingMatrix[0][1] += 1
+                elif (row[1] == "2"):
+                    trainingMatrix[1][1] += 1
+                elif (row[1] == "3"):
+                    trainingMatrix[2][1] += 1
+            elif (maximumResult == resultOfClass3):
+                if (row[1] == "1"):
+                    trainingMatrix[0][2] += 1
+                elif (row[1] == "2"):
+                    trainingMatrix[1][2] += 1
+                elif (row[1] == "3"):
+                    trainingMatrix[2][2] += 1
+
+    with open("testing.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        isHeader = True
+        for row in csv_reader:
+            if isHeader:
+                isHeader = False
+                continue
+            likelihood1 = class1.calculateLikelihhod(int(row[0]))
+            likelihood2 = class2.calculateLikelihhod(int(row[0]))
+            likelihood3 = class3.calculateLikelihhod(int(row[0]))
+            resultOfClass1 = (likelihood1 * class1.priors) / (
+                    (likelihood1 * class1.priors) + (likelihood2 * class2.priors) + (likelihood3 * class3.priors))
+            resultOfClass2 = ((likelihood2 * class2.priors) / (
+                    (likelihood1 * class1.priors) + (likelihood2 * class2.priors) + (likelihood3 * class3.priors)))
+            resultOfClass3 = ((likelihood3 * class3.priors) / (
+                    (likelihood1 * class1.priors) + (likelihood2 * class2.priors) + (likelihood3 * class3.priors)))
+
+            maximumResult = max(resultOfClass1, resultOfClass2, resultOfClass3)
+
+            if (maximumResult == resultOfClass1):
+                if (row[1] == "1"):
+                    testingMatrix[0][0] += 1
+                elif (row[1] == "2"):
+                    testingMatrix[1][0] += 1
+                elif (row[1] == "3"):
+                    testingMatrix[2][0] += 1
+            elif (maximumResult == resultOfClass2):
+                if (row[1] == "1"):
+                    testingMatrix[0][1] += 1
+                elif (row[1] == "2"):
+                    testingMatrix[1][1] += 1
+                elif (row[1] == "3"):
+                    testingMatrix[2][1] += 1
+            elif (maximumResult == resultOfClass3):
+                if (row[1] == "1"):
+                    testingMatrix[0][2] += 1
+                elif (row[1] == "2"):
+                    testingMatrix[1][2] += 1
+                elif (row[1] == "3"):
+                    testingMatrix[2][2] += 1
+
+        print("Matrix for training.csv -> ", trainingMatrix)
+        print("Matrix for testing.csv -> ", testingMatrix)
+
+
 def createGraph(filename):
     class1 = Class("1")
     class2 = Class("2")
@@ -68,6 +167,8 @@ def createGraph(filename):
     class2.calculateProps(lineCount)
     class3.calculateProps(lineCount)
 
+    #createConfusionMatrice(class1,class2,class3)
+
     for var in ages:
         likelihood1 = class1.calculateLikelihhod(var)
         likelihood2 = class2.calculateLikelihhod(var)
@@ -93,6 +194,8 @@ def createGraph(filename):
     plt.scatter(class1.ages, [-0.1] * len(class1.ages), color='r', marker="x")
     plt.scatter(class2.ages, [-0.2] * len(class2.ages), color='g', marker="x")
     plt.scatter(class3.ages, [-0.3] * len(class3.ages), color='b', marker="x")
+
+    plt.legend(["P(C=1|X)", "P(C=2|X)", "P(C=3|X)", "P(X|C=1)", "P(X|C=2)", "P(X|C=3)"])
 
     plt.show()
 
